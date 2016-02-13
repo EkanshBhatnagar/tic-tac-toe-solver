32ffeadc6c3880243fd89e67de8f22f2cf3ff7bb
Derek Felson <drfelson@gmail.com>
2016-02-13 18:52:56 -0500

    Added enums

---
+++ Types.h
@@ -0,0 +1,30 @@
+/*
+ * Types.h
+ *
+ *  Created on: 2016-02-13
+ *      Author: derek
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+namespace ttt
+{
+
+enum EndState
+{
+	NotOver,
+    XWin,
+	OWin,
+	Tie
+};
+
+enum Player
+{
+	XPlayer,
+	OPlayer
+};
+
+} /* namespace ttt */
+
+#endif /* TYPES_H_ */

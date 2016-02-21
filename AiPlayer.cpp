b84c7079af636b75db8bdc1abfec68012e83f795
Derek Felson <drfelson@gmail.com>
2016-02-21 15:07:15 -0500

    Uses simplified Node interface

--- AiPlayer.cpp
+++ AiPlayer.cpp
@@ -34,11 +34,7 @@ Move AiPlayer::nextMove(const GameBoard& board)
 
 	while (!fringe.empty())
 	{
-		if (fringe.front().getDepth() == 0 || fringe.front().isTerminalState())
-		{
-			// TODO: Evaluate the node heuristic and use it to update the parent
-		}
-		else if (fringe.front().hasNextNode())
+		if (fringe.front().hasNextNode())
 		{
 			// Expand the next node, and make that the top of the stack
 			fringe.front().expandNextNode(fringe);
@@ -62,25 +58,3 @@ Move AiPlayer::nextMove(const GameBoard& board)
 
 	return nodes.front().getBestMove();
 }
-
-/*
-01 function alphabeta(node, depth, α, β, maximizingPlayer)
-02      if depth = 0 or node is a terminal node
-03          return the heuristic value of node
-04      if maximizingPlayer
-05          v := -∞
-06          for each child of node
-07              v := max(v, alphabeta(child, depth - 1, α, β, FALSE))
-08              α := max(α, v)
-09              if β ≤ α
-10                  break (* β cut-off *)
-11          return v
-12      else
-13          v := ∞
-14          for each child of node
-15              v := min(v, alphabeta(child, depth - 1, α, β, TRUE))
-16              β := min(β, v)
-17              if β ≤ α
-18                  break (* α cut-off *)
-19          return v
-*/

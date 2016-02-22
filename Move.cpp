222edd19e97bb681c6a4bd6bb565ea0655b0281f
Derek Felson <drfelson@gmail.com>
2016-02-21 20:25:30 -0500

    Added operator<< for debugging

--- Move.cpp
+++ Move.cpp
@@ -6,9 +6,31 @@
  */
 
 #include <assert.h>
+#include <ostream>
 #include "Move.h"
+using namespace std;
 
 Move::Move(int x, int y) : x{x}, y{y}
 {
 	assert(x >= 0 && x <= 2 && y >= 0 && y <= 2);
+}
+
+bool operator==(const Move& lhs, const Move& rhs)
+{
+	return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+bool operator!=(const Move& lhs, const Move& rhs)
+{
+	return !(lhs == rhs);
+}
+
+ostream& Move::print(ostream& stream) const
+{
+	return stream << "Move{" << x << "," << y << "}";
+}
+
+ostream& operator<< (ostream& stream, const Move& move)
+{
+	return move.print(stream);
 }
--- Move.h
+++ Move.h
@@ -8,10 +8,17 @@
 #ifndef MOVE_H_
 #define MOVE_H_
 
+#include <iosfwd>
+
 struct Move final {
 	explicit Move(int x, int y);
 	int x;
 	int y;
+	std::ostream& print(std::ostream& stream) const;
 };
+
+bool operator==(const Move& lhs, const Move& rhs);
+bool operator!=(const Move& lhs, const Move& rhs);
+std::ostream& operator<< (std::ostream& stream, const Move& move);
 
 #endif /* MOVE_H_ */
